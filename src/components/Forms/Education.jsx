e6848df066c9da9a00d327b1f18862b9e63fcfe7
TipDevs <tipdevs@gmail.com>
2025-12-21 09:13:27 +0100

    feat: import new methods from useToggleForm, EducationForm and controlled shown form with useToggleForm inner method

--- src/components/Forms/Education.jsx
+++ src/components/Forms/Education.jsx
@@ -1,88 +1,61 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import EducationForm from "./EducationForm/FormComponent";
 import {
   faSchool,
   faCircleXmark,
   faEdit,
 } from "@fortawesome/free-solid-svg-icons";
 import useToggleForm from "../../Hooks/toggleForm";
+import { useState } from "react";
 export default function Education({
   cancelForm,
   onChange,
   storedEducation,
   addNewEducation,
   education,
+  deleteEducation,
+  editEducation,
 }) {
-  const { showForm, toggleForm } = useToggleForm(false);
+  const [educationToBeEdit, setEducationToBeEdit] = useState(null);
+  const {
+    showNewEntryForm,
+    toggleNewEntryForm,
+    showEditEntryForm,
+    toggleEditEntryForm,
+  } = useToggleForm(false);
   return (
     <>
       <section id="education">
         <h2>Education</h2>
-        {showForm ? (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              addNewEducation(toggleForm);
-              e.target.reset();
+        {showNewEntryForm ? (
+          <EducationForm
+            education={education}
+            toggler={toggleNewEntryForm}
+            callback={() => {
+              addNewEducation({ callBack: toggleNewEntryForm });
             }}
-            id="educationForm">
-            <FontAwesomeIcon
-              icon={faCircleXmark}
-              size="xl"
-              onClick={(e) => {
-                const targetElement = e.currentTarget;
-                cancelForm(targetElement, toggleForm);
-              }}
-              style={{ cursor: "pointer" }}
-            />
-            <label htmlFor="certification">
-              Certification:{" "}
-              <input
-                type="text"
-                name="certification"
-                value={education.certification}
-                placeholder="Specify certificate acquired"
-                onChange={onChange}
-                required
-              />
-            </label>
-            <label htmlFor="institution">
-              Institution:{" "}
-              <input
-                type="text"
-                name="institution"
-                value={education.institution}
-                placeholder="Enter the name of your Institution"
-                onChange={onChange}
-                required
-              />
-            </label>
-
-            <label htmlFor="convocationYear">
-              Admission Year:{" "}
-              <input
-                type="text"
-                name="convocationYear"
-                value={education.convocationYear.trim()}
-                id="convocationYear"
-                placeholder="Enter the year you got admission"
-                onChange={onChange}
-                required
-              />
-            </label>
-            <label htmlFor="graduationYear">
-              Graduation Year:{" "}
-              <input
-                type="text"
-                name="graduationYear"
-                value={education.graduationYear.trim()}
-                id="graduationYear"
-                placeholder="Enter the year you graduated"
-                onChange={onChange}
-                required
-              />
-            </label>
-            <button type="submit">Submit</button>
-          </form>
+            onChange={(e) => {
+              onChange(e);
+            }}
+            cancelForm={cancelForm}></EducationForm>
+        ) : showEditEntryForm ? (
+          <EducationForm
+            education={educationToBeEdit}
+            callback={() => {
+              editEducation({
+                educationToBeEdit: educationToBeEdit,
+                callBack: toggleEditEntryForm,
+              });
+            }}
+            toggler={toggleEditEntryForm}
+            onChange={(e) => {
+              const { name, value } = e.target;
+              setEducationToBeEdit((prev) => ({
+                ...prev,
+                [name]: value,
+              }));
+            }}
+            cancelForm={cancelForm}></EducationForm>
         ) : (
           <div id="education_list_table">
             <div id="education_list">
@@ -96,8 +69,7 @@ export default function Education({
               ) : (
                 storedEducation.map((education) => {
                   return (
-                    <ul
-                      key={education.certification + education.convocationYear}>
+                    <ul key={education.id}>
                       <li>
                         <ul>
                           <li>
@@ -109,7 +81,9 @@ export default function Education({
                               icon={faEdit}
                               size="xl"
                               onClick={(e) => {
-                                const targetElement = e.currentTarget;
+                                e.currentTarget;
+                                setEducationToBeEdit(() => education);
+                                toggleEditEntryForm();
                               }}
                               style={{ cursor: "pointer", color: "#fee2d8ff" }}
                             />
@@ -139,7 +113,8 @@ export default function Education({
                           icon={faCircleXmark}
                           size="xl"
                           onClick={(e) => {
-                            const targetElement = e.currentTarget;
+                            e.currentTarget;
+                            deleteEducation(education.id);
                           }}
                           style={{ cursor: "pointer", color: "#fee2d8ff" }}
                         />
@@ -149,7 +124,7 @@ export default function Education({
                 })
               )}
             </div>
-            <button onClick={toggleForm}>Add Education</button>
+            <button onClick={toggleNewEntryForm}>Add Education</button>
           </div>
         )}
       </section>
