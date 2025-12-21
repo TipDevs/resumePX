c32d6a3fa4a6170f5472b957c854f76228503631
TipDevs <tipdevs@gmail.com>
2025-12-21 08:59:03 +0100

    refactor: code refactor to use new features and methods

--- src/Hooks/toggleForm.jsx
+++ src/Hooks/toggleForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 export default function useToggleForm(initial = false) {
-  const [showForm, setShowForm] = useState(initial);
-  const toggleForm = () => setShowForm((prev) => !prev);
-  return { showForm, toggleForm };
+  const [showNewEntryForm, setshowNewEntryForm] = useState(initial);
+  const [showEditEntryForm, setShowEditEntryForm] = useState(initial);
+  const toggleNewEntryForm = () => setshowNewEntryForm((prev) => !prev);
+  const toggleEditEntryForm = () => setShowEditEntryForm((prev) => !prev);
+  return { showNewEntryForm, toggleNewEntryForm, showEditEntryForm, toggleEditEntryForm };
 }
