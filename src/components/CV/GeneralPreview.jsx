55d350f4d466aea80f3725c5efe7712afc0a5d39
TipDevs <tipdevs@gmail.com>
2025-12-22 03:00:02 +0100

    Add conditionals for general data content visibility, rearranged code, add and delete styles

--- src/components/CV/GeneralPreview.jsx
+++ src/components/CV/GeneralPreview.jsx
@@ -5,28 +5,13 @@ export default function GeneralPreview({ generalData }) {
   return (
     <>
       <div id="generalPreview">
-        {fullName !== "" ? <h3 id="fullName">{fullName}</h3> : ""}
-        {generalData.email !== "" ? (
-          <p id="email">
-            <span style={{ fontWeight: "bold" }}>Email:</span>{" "}
-            {generalData.email}
-          </p>
-        ) : (
-          ""
-        )}
-        {generalData.phone !== "" ? (
-          <p id="phoneNumber">
-            <span style={{ fontWeight: "bold" }}>Tel:</span> {generalData.phone}
-          </p>
-        ) : (
-          ""
-        )}
+        {fullName !== "" ? <h3 id="fullName">{fullName}</h3> : null}
 
-        {generalData.phone !== "" ? (
+        {generalData.address !== "" ? (
           <p
             id="address"
             style={{
-              fontSize: "1.5rem",
+              fontSize: "1.2rem",
               fontWeight: "lighter",
               marginTop: "3px",
               maxWidth: "50ch",
@@ -36,15 +21,38 @@ export default function GeneralPreview({ generalData }) {
             <span style={{ fontWeight: "bold" }}>Address:</span>{" "}
             {generalData.address}
           </p>
-        ) : (
-          ""
-        )}
+        ) : null}
+        {generalData.phone !== "" ? (
+          <p id="phoneNumber" style={{
+              fontSize: "1.2rem",
+              fontWeight: "lighter",
+              marginTop: "3px",
+              maxWidth: "50ch",
+              overflowWrap: "break-word",
+              textAlign: "center",
+            }}>
+            <span style={{ fontWeight: "bold" }}>Tel:</span> {generalData.phone}
+          </p>
+        ) : null}
+        {generalData.email !== "" ? (
+          <p id="email" style={{
+              fontSize: "1.05rem",
+              fontWeight: "lighter",
+              marginTop: "3px",
+              maxWidth: "50ch",
+              overflowWrap: "break-word",
+              textAlign: "center",
+            }}>
+            <span style={{ fontWeight: "bold", textDecoration: "none" }}>Email:</span>{" "}
+            <a href={generalData.email}>{generalData.email}</a>
+          </p>
+        ) : null}
         {generalData.description !== "" ? (
           <Fragment>
-            <h4>Professional Description</h4>
+            <h4 style={{color: "green"}}>Professional Description</h4>
             <hr
               style={{
-                width: "85%",
+                width: "90%",
                 height: "3px",
                 background: "#000000",
                 alignSelf: "center",
@@ -53,19 +61,17 @@ export default function GeneralPreview({ generalData }) {
             <p
               id="description"
               style={{
-                fontSize: "1.3rem",
+                fontSize: "1.06rem",
                 fontWeight: "lighter",
                 marginTop: "3px",
-                maxWidth: "50ch",
                 overflowWrap: "break-word",
                 textAlign: "center",
+                alignSelf: "center",
               }}>
               {generalData.description}
             </p>
           </Fragment>
-        ) : (
-          ""
-        )}
+        ) : null}
       </div>
     </>
   );
