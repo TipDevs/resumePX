292a0110bd4122a695c9ee90d0d7365ef88c4d08
TipDevs <tipdevs@gmail.com>
2025-12-23 00:30:22 +0100

    Add new prop parameter, remove jsPDF and its corresponding code and pass the new parameter for click event to print

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,15 @@
 import "../styles/Header.css";
-import jsPDF from "jspdf";
 import resumePX from "../../public/svgs/resumePX.svg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileExport } from "@fortawesome/free-solid-svg-icons";
-const generatePDF = () => {
-    const CV = new jsPDF('portrait', 'pt', 'a4');
-    CV.html(document.querySelector('#CV')).then(() => {
-      CV.save(`${document.querySelector("#fullName").textContent}.pdf`);
-    });
-  };
-export default function Header() {
+
+export default function Header({reactToPrintFn}) {
   return (
     <>
       <header>
         <img src={resumePX} alt="résuméPX logo" />
         <nav>
-          <button onClick={generatePDF}>
+          <button onClick={reactToPrintFn}>
             Export <FontAwesomeIcon icon={faFileExport} bounce size="xl" />
           </button>
         </nav>
