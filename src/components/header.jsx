3d0ca98883efc679fcd7a32b034255787347e668
TipDevs <tipdevs@gmail.com>
2025-12-22 02:41:31 +0100

    import jsPDF and add functionality to export button to export and download the CV in pdf format

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,21 @@
 import "../styles/Header.css";
+import jsPDF from "jspdf";
 import resumePX from "../../public/svgs/resumePX.svg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileExport } from "@fortawesome/free-solid-svg-icons";
+const generatePDF = () => {
+    const CV = new jsPDF('portrait', 'pt', 'a4');
+    CV.html(document.querySelector('#CV')).then(() => {
+      CV.save(`${document.querySelector("#fullName").textContent}.pdf`);
+    });
+  };
 export default function Header() {
   return (
     <>
       <header>
         <img src={resumePX} alt="résuméPX logo" />
         <nav>
-          <button>
+          <button onClick={generatePDF}>
             Export <FontAwesomeIcon icon={faFileExport} bounce size="xl" />
           </button>
         </nav>
