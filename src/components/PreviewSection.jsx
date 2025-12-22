7cd0388ddbab6b92912c72cfc4694abe56551591
TipDevs <tipdevs@gmail.com>
2025-12-22 03:06:30 +0100

    import SkillsPreviews component and invoke with props as well as refactor code for exporting

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -1,14 +1,22 @@
-import "../styles/PreviewSection.css"
+import "../styles/PreviewSection.css";
 import GeneralPreview from "./CV/GeneralPreview";
 import EducationPreview from "./CV/EducationPreview";
 import JobExperiencePreview from "./CV/JobExperiencePreview";
-export default function PreviewSection({storedEducation, storedJobs, ...props}) {
+import SkillsPreview from "./CV/SkillsPreview";
+export default function PreviewSection({
+  storedEducation,
+  storedJobs,
+  ...props
+}) {
   return (
     <section id="preview">
       <h2>Preview</h2>
-      <GeneralPreview generalData={props.generalData}/>
-      <EducationPreview storedEducation={storedEducation}/>
-      <JobExperiencePreview storedJobs={storedJobs}/>
+      <section id="CV">
+        <GeneralPreview generalData={props.generalData} />
+        <EducationPreview storedEducation={storedEducation} />
+        <JobExperiencePreview storedJobs={storedJobs} />
+        <SkillsPreview skills={props.skills} />
+      </section>
     </section>
   );
 }
