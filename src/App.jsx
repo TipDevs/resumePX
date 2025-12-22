ee3c2aaeb1c3d0f2db9c7953398e004a7eed0647
TipDevs <tipdevs@gmail.com>
2025-12-23 00:25:22 +0100

    import useRef, useReactToPrint. Add new variables and pass as props for printing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/header";
 import Editor from "./pages/Editor";
-
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
 function App() {
+  const contentRef = useRef();
+  const reactToPrintFn = useReactToPrint({
+    contentRef,
+    documentTitle: document.querySelector("#fullName")?.textContent || "mycv",
+  });
   return (
     <>
-      <Header />
+      <Header reactToPrintFn={reactToPrintFn} />
       <main>
-        <Editor/>
+        <Editor contentRef={contentRef} />
       </main>
     </>
   );
