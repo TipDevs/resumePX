21d636aad4ced805f9049775013277bdaf4302f2
TipDevs <tipdevs@gmail.com>
2025-12-23 00:29:13 +0100

    Add new prop parameter and pass it as ref value for printing

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -6,12 +6,13 @@ import SkillsPreview from "./CV/SkillsPreview";
 export default function PreviewSection({
   storedEducation,
   storedJobs,
+  contentRef,
   ...props
 }) {
   return (
     <section id="preview">
       <h2>Preview</h2>
-      <section id="CV">
+      <section id="CV" ref={contentRef}>
         <GeneralPreview generalData={props.generalData} />
         <EducationPreview storedEducation={storedEducation} />
         <JobExperiencePreview storedJobs={storedJobs} />
