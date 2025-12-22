93793229b7371602c0a66b216ade4f84b8ff3c66
TipDevs <tipdevs@gmail.com>
2025-12-22 03:05:17 +0100

    Add new component for previewing skills in list format

---
+++ src/components/CV/SkillsPreview.jsx
@@ -0,0 +1,20 @@
+export default function SkillsPreview({ skills }) {
+  return skills !== "" ? (
+    <div id="skillsPreview">
+      <h4 style={{color: "green"}}>Skills(Soft and Technical)</h4>
+      <hr
+        style={{
+          width: "90%",
+          height: "3px",
+          background: "#000000",
+          alignSelf: "center",
+        }}
+      />
+      <ul style={{padding: "5px 15px", paddingRight: "0"}}>
+        {skills.split("#").map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  ) : null;
+}
