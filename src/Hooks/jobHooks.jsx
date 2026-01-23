386ed461e9fb32c17d97a2c65b2b15fb93671efe
TipDevs <tipdevs@gmail.com>
2026-01-23 21:43:28 +0000

    feat: add new states and logics

--- src/Hooks/jobHooks.jsx
+++ src/Hooks/jobHooks.jsx
@@ -10,6 +10,7 @@ const initialJobExperience = {
 export default function useJobExperience() {
     const [jobForm, setJobForm] = useState(initialJobExperience);
     const [storedJobs, setStoredJobs] = useState([]);
+    const [formMode, setFormMode] = useState("off");
     const handleInputChange = (e) => {
         const {name, value} = e.target;
         setJobForm((prev) => ({
@@ -24,8 +25,45 @@ export default function useJobExperience() {
             endYear: "",
         }));
     }
+    const addNewJob = () => {
+        setFormMode("Add");
+        setJobForm((prev) => ({
+            ...prev,
+            id: crypto.randomUUID(),
+        }));
+    }
+    const editJob = (id) => {
+        setFormMode("Edit");
+        const jobToEdit = storedJobs.filter((job) => job.id === id);
+        setJobForm(jobToEdit[0]);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(formMode === "Add") {
+            setStoredJobs((prev) => ([
+                ...prev,
+                jobForm,
+            ]))
+        }
+        else if (formMode === "Edit") {
+            setStoredJobs((prev) => prev.map((job) => job.id === jobForm.id ? jobForm : job));
+        }
+        setJobForm(initialJobExperience);
+        setFormMode("off");
+    }
+    const cancelForm = () => {
+        setFormMode("off");
+        setJobForm(initialJobExperience);
+    }
     return {
         handleInputChange,
         toggleStillWorking,
+        addNewJob,
+        editJob,
+        jobForm,
+        onSubmit,
+        formMode,
+        storedJobs,
+        cancelForm,
     }
 }
