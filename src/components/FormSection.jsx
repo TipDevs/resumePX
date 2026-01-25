27bae2553f82b25f04a4da686eece39a83035098
TipDevs <tipdevs@gmail.com>
2026-01-25 06:43:13 +0000

    refactor: new approach to wire form logic with UI

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,31 +3,14 @@ import Education from "./Forms/Education";
 import JobExperience from "./Forms/JobExperience";
 import Skills from "./Forms/Skills";
 import "../styles/FormSection.css";
-export default function FormSection({storedEducation, storedJobs, eventHandler, ...props }) {
+export default function FormSection({general, educationFormat, jobFormat, skillsFormat}) {
   return (
     <section id="input_field">
       <h2>Input Field</h2>
-      <General generalData={props.generalData} onChange={eventHandler.generalFormInput} />
-      <Education
-        education={props.education}
-        storedEducation={storedEducation}
-        onChange={eventHandler.educationFormInput}
-        addNewEducation={eventHandler.addNewEducation}
-        cancelForm={eventHandler.cancelForm}
-        deleteEducation={eventHandler.deleteEducation}
-        editEducation={eventHandler.editEducation}
-      />
-      <JobExperience
-        jobExperience={props.jobExperience}
-        storedJobs={storedJobs}
-        onChange={eventHandler.jobExperienceFormInput}
-        stillWorkingCheckedForNewJob={eventHandler.stillWorkingCheckedForNewJob}
-        addNewJob={eventHandler.addNewJob}
-        cancelForm={eventHandler.cancelForm}
-        deleteJobExperience={eventHandler.deleteJobExperience}
-        editJobExperience={eventHandler.editJobExperience}
-      />
-      <Skills skills={props.skills} onChange={eventHandler.skillFormInput}/>
+      <General generalData={general.generalForm} onChange={general.handleInputChange}/>
+      <Education educationFormat={educationFormat}/>
+      <JobExperience jobFormat={jobFormat}/>
+      <Skills skillsFormat={skillsFormat}/>
     </section>
   );
 }
