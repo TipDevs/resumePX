0d3a4e61d5ae72290599de74b5b31af02ffc619d
TipDevs <tipdevs@gmail.com>
2026-01-25 06:48:05 +0000

    feat: import new implementation, manage state/logic and UI connectivity

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,3 +1,16 @@
 import FormSection from "../components/FormSection";
 import PreviewSection from "../components/PreviewSection";
+import useGeneral from "../Hooks/generalHooks";
 import useJobExperience from "../Hooks/jobHooks";
+import useEducation from "../Hooks/educationHooks";
+import useSkills from "../Hooks/skillsHooks";
+export default function Editor({contentRef}) {
+    const general = useGeneral();
+    const jobFormat = useJobExperience();
+    const educationFormat = useEducation();
+    const skillsFormat = useSkills();
+    return <>
+    <FormSection general={general} jobFormat={jobFormat} educationFormat={educationFormat} skillsFormat={skillsFormat}/>
+    <PreviewSection generalData={general.generalForm} storedEducation={educationFormat.storedEducations} storedJobs={jobFormat.storedJobs} skills={skillsFormat.skillsForm} contentRef={contentRef}/>
+    </>
+}
