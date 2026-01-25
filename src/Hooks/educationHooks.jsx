55939fe200cc19d0e776e65d33fa492562adb3b3
TipDevs <tipdevs@gmail.com>
2026-01-25 06:11:39 +0000

    feat: add new state for controlling form mode and new logic for CRUD implementation

--- src/Hooks/educationHooks.jsx
+++ src/Hooks/educationHooks.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import JobForm from "../components/Forms/JobForm/FormComponent";
 const initialEducation = {
     institution: "",
     certification: "",
@@ -8,6 +9,7 @@ const initialEducation = {
 export default function useEducation() {
     const [educationForm, setEducationForm] = useState(initialEducation);
     const [storedEducations, setStoredEducation] = useState([]);
+    const [formMode, setFormMode] = useState("off");
     const handleInputChange = (e) => {
         const {name, value} = e.target;
         setEducationForm((prev) => ({
@@ -15,7 +17,48 @@ export default function useEducation() {
             [name] : value,
         }));
     }
+    const addNewEducation = () => {
+        setFormMode("Add");
+        setEducationForm((prev) => ({
+            ...prev,
+            id: crypto.randomUUID(),
+        }))
+    }
+    const editEducation = (id) => {
+        setFormMode("Edit");
+        const educationToEdit = storedEducations.find((education) => education.id === id);
+        setEducationForm(educationToEdit);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(formMode === "Add") {
+            setStoredEducation((prev) => ([
+                ...prev,
+                educationForm,
+            ]))
+        }
+        else if(formMode === "Edit") {
+            setStoredEducation((prev) => prev.map((education) => education.id === educationForm.id ? educationForm : education));
+        }
+        setEducationForm(initialEducation);
+        setFormMode("off");
+    }
+    const deleteEducation = (id) => {
+        setStoredEducation((prev) => prev.filter((education) => education.id !== id))
+    }
+    const cancelForm = () => {
+        setFormMode("off");
+        setEducationForm(initialEducation);
+    }
     return {
         handleInputChange,
+        addNewEducation,
+        editEducation,
+        onSubmit,
+        deleteEducation,
+        cancelForm,
+        formMode,
+        educationForm,
+        storedEducations,
     }
 }
