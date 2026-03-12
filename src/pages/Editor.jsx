9c745c6ff05b4b36217838eb01c39bb8be1d7f93
TipDevs <tipdevs@gmail.com>
2026-03-12 14:22:05 +0100

    fix: remove unnecessary import hooks which cause whole app to re-render as well as unused parameters after.

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,16 +1,14 @@
 import FormSection from "../components/FormSection";
 import PreviewSection from "../components/PreviewSection";
-import useGeneral from "../Hooks/generalHooks";
-import useJobExperience from "../Hooks/jobHooks";
-import useEducation from "../Hooks/educationHooks";
-import useSkills from "../Hooks/skillsHooks";
-export default function Editor({contentRef, showPreview, toggleShowPreview}) {
-    const general = useGeneral();
-    const jobFormat = useJobExperience();
-    const educationFormat = useEducation();
-    const skillsFormat = useSkills();
-    return <>
-    <FormSection general={general} jobFormat={jobFormat} educationFormat={educationFormat} skillsFormat={skillsFormat} showPreview={showPreview}/>
-    <PreviewSection generalData={general.generalForm} storedEducation={educationFormat.storedEducations} storedJobs={jobFormat.storedJobs} skills={skillsFormat.skillsForm} contentRef={contentRef} showPreview={showPreview} toggleShowPreview={toggleShowPreview}/>
+export default function Editor({ contentRef, showPreview, toggleShowPreview }) {
+  return (
+    <>
+      <FormSection showPreview={showPreview} />
+      <PreviewSection
+        contentRef={contentRef}
+        showPreview={showPreview}
+        toggleShowPreview={toggleShowPreview}
+      />
     </>
+  );
 }
