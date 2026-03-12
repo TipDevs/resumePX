3b105863aa0acfbb308dea3192c9c2a6217c3ec6
TipDevs <tipdevs@gmail.com>
2026-03-12 20:31:19 +0100

    refactor: restructure component - removed unused parameters - ensured FormSection only render form UI components

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,14 +3,14 @@ import Education from "./Forms/Education";
 import JobExperience from "./Forms/JobExperience";
 import Skills from "./Forms/Skills";
 import "../styles/FormSection.css";
-export default function FormSection({general, educationFormat, jobFormat, skillsFormat, showPreview}) {
+export default function FormSection({showPreview}) {
   return (
     <section id="input_field" className={showPreview ? "on_preview" : ""}>
       <h2>Input Field</h2>
-      <General generalData={general.generalForm} onChange={general.handleInputChange}/>
-      <Education educationFormat={educationFormat}/>
-      <JobExperience jobFormat={jobFormat}/>
-      <Skills skillsFormat={skillsFormat}/>
+      <General/>
+      <Education/>
+      <JobExperience/>
+      <Skills/>
     </section>
   );
 }
