57c9e059a836957dc233eae44e21479e1dc8f658
TipDevs <tipdevs@gmail.com>
2026-03-12 20:22:27 +0100

    refactor: restructure compponent - removed the print function dependency injection and let the component own the functionality itself - rearranged the component structure for readability - passed in a new parameter contentRef

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,24 @@
 import "../styles/Header.css";
-import resumePX from "../../public/svgs/resumePX.svg"
+import resumePX from "/svgs/resumePX.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileExport, faEye } from "@fortawesome/free-solid-svg-icons";
-
-export default function Header({reactToPrintFn, toggleShowPreview, showPreview}) {
+import { useReactToPrint } from "react-to-print";
+export default function Header({ contentRef, toggleShowPreview, showPreview }) {
+  const reactToPrintFn = useReactToPrint({
+    contentRef,
+    documentTitle: "mycv",
+  });
   return (
     <>
       <header>
         <img src={resumePX} alt="résuméPX logo" />
         <nav>
-          <button disabled={showPreview} onClick={toggleShowPreview} id="preview_button"><FontAwesomeIcon icon={faEye} size="xl" /></button>
+          <button
+            disabled={showPreview}
+            onClick={toggleShowPreview}
+            id="preview_button">
+            <FontAwesomeIcon icon={faEye} size="xl" />
+          </button>
           <button onClick={reactToPrintFn} disabled={showPreview}>
             Export <FontAwesomeIcon icon={faFileExport} bounce size="xl" />
           </button>
