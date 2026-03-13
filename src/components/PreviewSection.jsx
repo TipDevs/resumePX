5c6109e2bfab04664309d0b519a09da40735fa3b
TipDevs <tipdevs@gmail.com>
2026-03-13 06:28:00 +0100

    refactor: restructure component     - removed unused parameters and props     - ensured PreviewSection only render preview UI components

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -6,11 +6,7 @@ import SkillsPreview from "./CV/SkillsPreview";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faCircleXmark} from "@fortawesome/free-solid-svg-icons";
 export default function PreviewSection({
-  storedEducation,
-  storedJobs,
   contentRef,
-  skills,
-  generalData,
   showPreview,
   toggleShowPreview
 }) {
@@ -19,10 +15,10 @@ export default function PreviewSection({
       <h2>Preview</h2>
       <FontAwesomeIcon icon={faCircleXmark} size="2xl" style={{alignSelf: "flex-start", position: "absolute", top: "3%", left: "6%"}} onClick={toggleShowPreview} id="buttonClosingPreview"/>
       <section id="CV" ref={contentRef}>
-        <GeneralPreview generalData={generalData} />
-        <EducationPreview storedEducation={storedEducation} />
-        <JobExperiencePreview storedJobs={storedJobs} />
-        <SkillsPreview skills={skills} />
+        <GeneralPreview/>
+        <EducationPreview/>
+        <JobExperiencePreview/>
+        <SkillsPreview/>
       </section>
     </section>
   );
